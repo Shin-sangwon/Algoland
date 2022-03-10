fb88fc1373f29e897419195cb0dd19bd9f90719f
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-03-10 17:58:07 +0900

    [Silver II] Title: 연속합, Time: 840 ms, Memory: 108852 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.math.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		int arr[] = new int[cnt+1];
+		for(int i = 1; i <= cnt; i++) arr[i] = sc.nextInt();
+		int dp[] = new int[cnt+1];
+		dp[0] = Integer.MIN_VALUE;
+		dp[1] = arr[1];
+		for(int i = 2; i <=cnt; i++) {
+			dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
+		}
+		Arrays.sort(dp);
+		System.out.println(dp[cnt]);
+		
+	}
+}
