86f24d92201dc595460eb72410d7bfdb66fc10d0
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-03-15 16:29:12 +0900

    [Silver I] Title: 미로 탐색, Time: 252 ms, Memory: 18416 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Node {
+	
+	int x;
+	int y;
+	
+	public Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+}
+
+
+public class Main {
+	
+	static int n,m;
+	static int[][] arr = new int[101][101];
+	static int dx[] = {-1, 1, 0, 0};
+	static int dy[] = {0, 0, -1, 1};
+	
+	static int bfs(int x, int y) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(x,y));
+		
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			x = node.getX();
+			y = node.getY();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
+				if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+				if(arr[nx][ny] == 0) continue;
+				if(arr[nx][ny] == 1) {
+					arr[nx][ny] = arr[x][y] + 1;
+					q.offer(new Node(nx, ny));
+				}
+			}
+		}
+		
+		return arr[n-1][m-1];
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt(); m = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 0; i < n; i++) {
+			String str = sc.nextLine();
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = str.charAt(j) - '0';
+			}
+		}
+		
+		System.out.println(bfs(0, 0));
+	}
+
+}
