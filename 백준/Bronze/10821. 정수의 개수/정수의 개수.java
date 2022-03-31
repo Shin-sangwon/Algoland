c5223a6e1281647ccedecc28dda3c296fc169901
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-03-31 13:12:20 +0900

    [Bronze II] Title: 정수의 개수, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		String str[] = br.readLine().split(",");	
+				
+		System.out.println(str.length);
+	}
+}
