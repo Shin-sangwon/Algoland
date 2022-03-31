e3bd4fc05521adc5f3b6c5c7d107b9af2baa6ba6
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-03-31 12:53:35 +0900

    [Bronze I] Title: 팰린드롬인지 확인하기, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		String str = br.readLine();
+		int ans = 1;
+		for(int i = 0; i < str.length()/2; i++) {
+			if(str.charAt(i) != str.charAt(str.length()-1-i)) {
+				ans = 0;
+				break;
+			}		
+		}
+		System.out.println(ans);
+	}
+}
