0c98cea1796a5e5a19ab03132873f9a7b62746c6
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-05-12 23:22:08 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		bw.write("         ,r\'\"7"); bw.newLine();		
+		bw.write("r`-_   ,'  ,/"); bw.newLine();
+		bw.write(" \\. \". L_r'"); bw.newLine();
+		bw.write("   `~\\/"); bw.newLine();
+		bw.write("      |"); bw.newLine();
+		bw.write("      |");
+		
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
