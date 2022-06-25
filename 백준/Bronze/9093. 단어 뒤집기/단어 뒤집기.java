404bd728ace353f03778f519ab16f3b7ce7d03c3
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-06-26 01:26:47 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 464 ms, Memory: 41772 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,25 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		String str = br.readLine();
-		for(int i = 0; i < Integer.parseInt(str); i++) {
-			String tmp1 = br.readLine();
-			String ans = "";
-			StringTokenizer st = new StringTokenizer(tmp1, " ");
-			while(st.hasMoreTokens()) {
-				String tmp2 = st.nextToken();
-				for(int j = tmp2.length()-1; j >= 0; j--) {
-					ans += tmp2.charAt(j);
-				}
-				ans += " ";
-			}
-			System.out.println(ans);
-			ans = "";
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb;
+		StringBuilder reversesb;
+		int n = Integer.parseInt(br.readLine());
+		
+		while(n-->0) {
+			st = new StringTokenizer(br.readLine(), " ");	
+			while(st.hasMoreTokens()) {				
+				sb = new StringBuilder();
+				sb.append(st.nextToken()).reverse();
+				sb.append(" ");
+				bw.write(sb.toString());
+			}			
+			bw.newLine();
 		}
+		
+		br.close();
+		bw.close();
+			
 	}
 }
