6c5a368707d72611655a338bcecc34dec024ca91
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-08-06 22:44:07 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 21416 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,28 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
-	
-		
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int ans = 0;
-		int cnt = Integer.parseInt(br.readLine());
-		Stack<Integer> s = new Stack<>();
-		for(int i = 0; i < cnt; i++) {
-			int tmp = Integer.parseInt(br.readLine());
-			
-			if(tmp == 0) {
-				s.pop();
-			}else {
-				s.push(tmp);
-			}
-		}
-		
-		for(int x : s) ans += x;
-		
-		System.out.println(ans);
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Integer> stack = new Stack<>();
+
+        int totalCase = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        while(totalCase-->0) {
+            int tmp = Integer.parseInt(br.readLine());
+
+            if(tmp != 0) {
+                stack.push(tmp);
+                continue;
+            }
+            
+            stack.pop();
+        }
+
+        for(int x : stack) ans += x;
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
