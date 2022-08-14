022291e66d8247651b559db643cf835041c5f813
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-08-14 21:47:19 +0900

    [Silver III] Title: 문자열 집합, Time: 348 ms, Memory: 39052 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,38 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		HashSet<String> al = new HashSet<>();
-		st = new StringTokenizer(br.readLine());
-		int ans = 0;
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		
-		while(n-- != 0) {
-			al.add(br.readLine());
-		}
-		
-		
-		while(m-- != 0) {
-			if(al.contains(br.readLine())) {
-				ans++;
-			}
-		}
-		
-		bw.write(ans+"");
-		bw.flush();
-		bw.close();
-		br.close();
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Boolean> map = new HashMap<String, Boolean>();
+        int ans = 0;
+
+        while(n--> 0) {
+            map.put(br.readLine(), true);
+        }
+
+        while(m--> 0) {
+            if(map.get(br.readLine()) != null) ans += 1;
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
 }
