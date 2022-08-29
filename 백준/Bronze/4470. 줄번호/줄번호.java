2a56209d6ea86cb32d77fe37b25b4e47a34b4c57
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-08-29 22:13:05 +0900

    [Bronze IV] Title: 줄번호, Time: 72 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        while(n-->0) {
+            sb.append(cnt++ + ". ").append(br.readLine()+"\n");
+
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
