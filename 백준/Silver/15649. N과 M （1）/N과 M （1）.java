301485a5a68864cd1934692ffeefccfdf95b777d
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-09-14 22:53:05 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 51440 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,47 +1,60 @@
-import java.util.*;
-import java.math.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 
 public class Main {
-	
-	static int arr[];
-	static boolean checker[];
-	static int n;
-	static int m;
-	
-	static void seq(int x) {
-		if(x == m) {
-			for(int i = 0; i < m; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i = 1; i <= n; i++) {
-			if(!checker[i]) {
-				arr[x] = i;
-				checker[i] = true;
-				seq(x+1);
-				checker[i] = false;
-			}
-		}
-		
-	}
-	
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());		
-		arr = new int[10];
-		checker = new boolean[10];
-		seq(0);
-		
-	}
-			
+
+    static int n,m;
+    static int arr[];
+    static boolean checker[];
+    static StringBuilder sb;
+
+    static void recursive(int x) {
+
+        if(x == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(!checker[i]) {
+                arr[x] = i;
+                checker[i] = true;
+                recursive(x + 1);
+                checker[i] = false;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str[] = br.readLine().split(" ");
+
+        sb = new StringBuilder();
+        n = Integer.parseInt(str[0]);
+        m = Integer.parseInt(str[1]);
+        arr = new int[10];
+        checker = new boolean[10];
+        recursive(0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+
+
+    }
 }
