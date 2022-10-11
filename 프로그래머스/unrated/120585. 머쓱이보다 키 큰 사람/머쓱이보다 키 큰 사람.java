3e13da4c4174fb862cfcae160721cb4f4d8ed28b
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-10-11 16:22:49 +0900

    [unrated] Title: 머쓱이보다 키 큰 사람, Time: 1.95 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        
+        return (int) Arrays.stream(array)
+            .filter(x -> x > height)
+            .count();
+    }
+}
