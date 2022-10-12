9933fa8f64f8a39776ae0057ddb28f46eb23ac9e
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-10-12 10:34:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.48 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,14 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
+        final String target = "Kim";
         
-        for(int i = 0; i < seoul.length; i++) {
-            if(seoul[i].equals("Kim")) {
-                answer = "김서방은 " + i + "에 있다";
-                break;
-            }
-        }
+        int idx = IntStream.range(0, seoul.length)
+            .filter(x -> target.equals(seoul[x]))
+            .findFirst()
+            .orElse(0);
         
-        return answer;
+        return "김서방은 " + idx + "에 있다";
     }
 }
