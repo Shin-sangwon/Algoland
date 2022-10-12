af07626112c5c8a5ba6e85b924eb76286a727a55
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-10-12 09:12:27 +0900

    [level 1] Title: 평균 구하기, Time: 2.16 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+       
+        return Arrays.stream(arr)
+            .average()
+            .orElse(0);
+    }
+}
