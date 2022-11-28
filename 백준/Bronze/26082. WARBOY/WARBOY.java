19ae8def352bd2fefb28f114812119fd02e03570
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-11-28 21:24:18 +0900

    [Bronze V] Title: WARBOY, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int other = b / a;
+        bw.write(String.valueOf(other * 3 * c));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
