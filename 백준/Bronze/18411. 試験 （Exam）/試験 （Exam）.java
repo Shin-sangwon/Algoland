80de273168b8018fcdde2e7709dfdb7bafbd3dfc
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-12-09 15:03:16 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 232 ms, Memory: 18820 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.Stream;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int answer = Stream.of(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .skip(1)
+                .sum();
+        
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
