504b94a07d4fedc7618ce5c71e9a3dbd43854640
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2022-12-15 20:16:14 +0900

    [Bronze III] Title: 선린인터넷고등학교 교가, Time: 84 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        br.readLine();
+        sb.append(br.readLine());
+
+        bw.write(sb.substring(sb.length()-5, sb.length()));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
