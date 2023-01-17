52173e6106586570c027c3256a98505db5a3f5c7
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-01-17 23:21:33 +0900

    [Silver II] Title: 최대 힙, Time: 304 ms, Memory: 27936 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,28 +1,47 @@
-import java.util.*;
-import java.math.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
-	
-		
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-		
-		int cnt = sc.nextInt();
-		for(int i = 0; i < cnt; i++) {
-			int tmp = sc.nextInt();
-			
-			if(tmp == 0) {
-				if(q.isEmpty()) {
-					System.out.println(0);
-				}else {
-					System.out.println(q.poll());
-				}
-			}
-			
-			q.add(tmp);
-		}
-	}
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        while(n-->0) {
+
+            //0 -> 큰 값 출력, 배열에서 제거
+            //그 외 -> 삽입
+            int tmp = Integer.parseInt(br.readLine());
+            if(tmp == 0) {
+
+                if(q.isEmpty()) {
+                    sb.append("0\n");
+                    continue;
+                }
+
+                sb.append(q.poll()+"\n");
+                continue;
+            }
+
+            q.add(tmp);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
 }
