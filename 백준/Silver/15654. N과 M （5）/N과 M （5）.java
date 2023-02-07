60e918efd785349264457b13d37e262710cf729c
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-02-07 19:09:12 +0900

    [Silver III] Title: N과 M (5), Time: 388 ms, Memory: 37508 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	static int[] arr;
+	static int[] num;
+	static boolean[] visited;
+	static int n,m;
+	
+	private static void recursive(int depth) {
+		
+		
+		if(m == depth) {
+			for(int i = 0; i < m; i++) {
+				sb.append(num[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			if(visited[i]) continue;
+			num[depth] = arr[i];
+			visited[i] = true;
+			recursive(depth + 1);
+			visited[i] = false;
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = Stream.of(br.readLine().split(" "))
+						  .mapToInt(Integer::parseInt)
+						  .sorted()
+						  .toArray();
+		
+		num = new int[m];
+		visited = new boolean[n];
+		recursive(0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+
+}
