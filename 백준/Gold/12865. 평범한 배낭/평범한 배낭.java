c152db7f2809de230bea8b5f444a2bed680da38e
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-03-30 21:25:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 51396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,33 +1,40 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-		
-	public static void main(String[] args) throws IOException {
-		
+
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
 		
-		st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int dp[] = new int[k+1];
+		int n = Integer.parseInt(st.nextToken()); // 개수
+		int totalWeight = Integer.parseInt(st.nextToken()); // 한계 무게
 		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int weight = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-			for(int j = k; j >= weight; j--) {
-				dp[j] = Math.max(dp[j], dp[j-weight] + value);
+		int[] weights = new int[n+1]; 
+		int[] profits = new int[n+1];
+		
+		int[][] dp = new int[n+1][totalWeight + 1];
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			weights[i] = Integer.parseInt(st.nextToken());
+			profits[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i <= n; i++) { // i : 물건
+			for(int w = 1; w <= totalWeight; w++) { // w : 가방의 무게
+				// 해당 물건의 무게가 w를 초과하는지
+				if(weights[i] > w) {
+					dp[i][w] = dp[i-1][w];
+				}else {
+					dp[i][w] = Math.max(dp[i-1][w], profits[i] + dp[i-1][w-weights[i]]);
+				}
 			}
 		}
 		
-		bw.write(dp[k]+"");
-		bw.flush();
-		bw.close();
-		
+		System.out.println(dp[n][totalWeight]);
 	}
+
 }
