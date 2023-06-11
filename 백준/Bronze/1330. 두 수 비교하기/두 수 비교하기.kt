8ba62f549c906b21da0e12060623005e742da9c5
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-06-11 23:01:25 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 88 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -1,24 +1,22 @@
-fun main() {
-        val input = readLine();
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
 
-        if(input != null) {
-                val numbers = input.split(" ");
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+        val st = StringTokenizer(readLine());
+        val left = Integer.parseInt(st.nextToken());
+        val right = Integer.parseInt(st.nextToken());
 
-                if(numbers.size == 2) {
-                        val left = numbers[0].toInt();
-                        val right = numbers[1].toInt();
+        print(calc(left, right));
+}
 
-                        if(left != null && right != null) {
-                                val answer = when {
-                                        left > right -> ">"
-                                        left < right -> "<"
-                                        else -> "=="
-                                }
+fun calc(left: Int, right: Int): String {
 
-                                print(answer);
-                        }
-                }
+        val answer = when{
+                left > right -> ">"
+                left < right -> "<"
+                else -> "=="
         }
 
-
+        return answer;
 }
