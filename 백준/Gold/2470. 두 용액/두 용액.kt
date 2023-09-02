140d5850d5b0d3d391faad1d513a8125afdb6cee
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-09-02 19:20:28 +0900

    [Gold V] Title: 두 용액, Time: 352 ms, Memory: 36788 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.kt
@@ -0,0 +1,60 @@
+package ryu.week1
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.abs
+
+var n = 0
+val br = BufferedReader(InputStreamReader(System.`in`))
+var answer = Int.MAX_VALUE
+lateinit var arr: IntArray
+lateinit var node: Pair<Int, Int>
+
+fun main() {
+
+    init()
+    calc()
+
+    print("${node.first} ${node.second}")
+}
+
+fun init() {
+    n = br.readLine().toInt()
+    arr = IntArray(n)
+    val st = StringTokenizer(br.readLine())
+
+    for(i in 0 until n) {
+        arr[i] = st.nextToken().toInt()
+    }
+
+    arr.sort()
+}
+
+fun calc() {
+
+    var left = 0
+    var right = n-1
+
+    while(left < right) {
+        val value = arr[left] + arr[right]
+
+        if(value == 0) {
+            node = Pair(arr[left], arr[right])
+            return
+        }
+
+        if(abs(value) < answer) {
+            node = Pair(arr[left], arr[right])
+            answer = abs(value)
+        }
+
+        if(value < 0) {
+            left++
+        }else if(value > 0) {
+            right--
+        }
+    }
+}
+
+
