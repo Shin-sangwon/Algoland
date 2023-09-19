e1be3678256a29f820a99858dadf7a847d2c222b
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-09-19 23:41:30 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 88 ms, Memory: 12192 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    print(n / 11 * 10)
+
+}
+
+
+
+
+
+
+
