9c635329bbbfb6b9d66e741e302645e493008795
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-10-09 21:04:51 +0900

    [Bronze V] Title: Java Warriors, Time: 92 ms, Memory: 12180 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val n = br.readLine().toInt()
+
+    print(n * 4000)
+
+}
