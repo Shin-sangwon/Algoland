a8e0668e7e386fcd81e53704477d6f534a677352
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-11-25 21:34:16 +0900

    [Gold V] Title: 강의실 배정, Time: 1072 ms, Memory: 145192 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val n = readLine().toInt()
+    val pq: PriorityQueue<Pair<Int, Int>> = PriorityQueue { o1, o2 -> o1.second - o2.second }
+    val arr = Array(n) {Pair(0, 0)}
+
+    for(i in 0 until n) {
+        val pair:Pair<Int, Int> = readLine().split(" ").let { (a, b) -> Pair(a.toInt(), b.toInt()) }
+        arr[i] = pair
+    }
+
+    arr.apply { sortBy { it.first } }.forEach { lecture ->
+        if(pq.isNotEmpty() && lecture.first >= pq.peek().second) {
+            pq.poll()
+        }
+        pq.add(lecture)
+     }
+
+    print(pq.size)
+
+}
