83a459c92f895fed6c696a69f8c631116577b298
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2024-02-24 00:59:21 +0900

    [Bronze IV] Title: Andando no tempo, Time: 164 ms, Memory: 24052 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val arr = readLine().split(" ").map { it.toInt() }.toIntArray().sorted()
+
+    val ans = arr[0] == arr[1] || arr[1] == arr[2] || arr[0] + arr[1] == arr[2]
+
+    print(if (ans) "S" else "N")
+}
